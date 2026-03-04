36b78c83059ccbc5e6fe4180d397bc1820da3882
gaesigua <gasigwatin@gmail.com>
2026-03-04 09:47:24 +0200

    added trivia bot and renamed a few repos

---
+++ Project 1 JavaScript Trivia Bot/index.js
@@ -0,0 +1,38 @@
+let greetingMessage;
+greetingMessage = "Hello! I'm your coding fun fact guide!";
+console.log(greetingMessage);
+
+let botName;
+botName = "andika";
+
+let botLocation;
+botLocation = "KK 1 Ave, Kigali";
+
+let favoriteLanguage;
+favoriteLanguage = "Kinyarwanda";
+
+let botGreeting = "My name is " + botName + " and I live on " + botLocation + ".";
+
+console.log(botGreeting);
+
+let languagePreference = "My favorite programming language is " + favoriteLanguage + ".";
+
+console.log(languagePreference);
+
+let codingFact;
+codingFact = "Fun fact about me: The only language I understand is the entire world is " + favoriteLanguage + ".";
+
+console.log(codingFact);
+
+codingFact = "But now in 2030, my favorite language now is " + favoriteLanguage + ".";
+
+console.log(codingFact);
+
+codingFact = "But in 2040, my favorite language will be " + favoriteLanguage + ".";
+
+console.log(codingFact);
+
+let farewellStatement = "It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".";
+
+console.log(farewellStatement);
+
---
+++ Project 1 Sentence Maker/index.js
@@ -0,0 +1,33 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "gross";
+noun = "Hund";
+verb = "ist";
+place = "Berlin";
+adjective2 = "süß";
+noun2 = "Katze";
+
+let firstStory;
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+
+adjective = "klein";
+noun = "Frau";
+verb = "ist";
+place = "Frankfurt";
+adjective2 = "freundlich";
+noun2 = "Katze";
+
+let secondStory;
+
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
+
